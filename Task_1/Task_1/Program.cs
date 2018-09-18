380a5bfb2cf1539be76da1dd6ccf06410737cc38
ivanternovyi <ivanternovyi@gmail.com>
2018-09-18 23:21:23 +0300

    ReadData method that reads many triangles from file

--- Task_1/Task_1/Program.cs
+++ Task_1/Task_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Task_1.Classes;
-using Task_1.Structures;
 
 namespace Task_1
 {
@@ -8,11 +9,37 @@ namespace Task_1
     {
         public static void Main(string[] args)
         {
-            ColoredSide cs = default(ColoredSide);
-            Triangle t = new Triangle();
-            Console.WriteLine(cs.Length);
-            Console.WriteLine(t);
+            // Ex: white, 10, red, 2, green, 5 then another triangle write in the next line
+//            var arrTriangle = ReadData("data.txt");
+//            foreach (var triangle in arrTriangle)
+//            {
+//                Console.WriteLine(triangle);
+//                triangle.Save("info.txt");
+//            }
+           // Console.WriteLine(tr.Parse("white, 10, red, 2, green, 5"));
             Console.ReadKey();
+        }
+
+        public static IEnumerable<Triangle> ReadData(string filename)
+        {
+            var res = new List<Triangle>();
+            string[] data;
+            try
+            {
+                data = File.ReadAllLines(filename);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            foreach (var line in data)
+            {
+                var tempTriangle = new Triangle();
+                res.Add(tempTriangle.Parse(line));
+            }
+
+            return res;
         }
     }
 }
