db5b8ce7a17a91b36fba4d49fb2d58301f1b4b1a
ivanternovyi <ivanternovyi@gmail.com>
2018-10-13 15:38:44 +0300

    Added serialization for broken line

---
+++ WpfShapes/WpfShapes/Utils/Serialization.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+using WpfShapes.Classes;
+
+namespace WpfShapes.Utils
+{
+    public static class Serialization
+    {
+        public static void Serialize(string filename, IEnumerable<BrokenLine> arr)
+        {
+            var xml = new XmlSerializer(typeof(List<BrokenLine>));
+            using (var fs = new FileStream(filename, FileMode.Create))
+            {
+                xml.Serialize(fs, arr);
+            }
+        }
+
+        public static IEnumerable<BrokenLine> Deserialize(string filename)
+        {
+            var res = new List<BrokenLine>();
+            var xml = new XmlSerializer(typeof(List<BrokenLine>));
+            using (var fs = new FileStream(filename, FileMode.Open))
+            {
+                res = (List<BrokenLine>) xml.Deserialize(fs);
+            }
+            return res;
+        }
+    }
+}
