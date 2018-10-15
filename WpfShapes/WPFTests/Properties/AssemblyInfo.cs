81a179c58b0c52947b76f02436858cd8c17cb19c
MaryanTutunnyk <maryan.tutunnyk@gmail.com>
2018-10-15 17:43:39 +0300

    Add unit tests.

---
+++ WpfShapes/WPFTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WPFTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WPFTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee3c2f82-93ff-4fa3-86cd-7b95e4ba582a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WpfShapes/WPFTests/UnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfShapes.Utils;
+using System.Windows.Media;
+
+namespace WPFTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        private const double firstPointCoordinate = 0.0;
+        private const double secondPointCoordinate = 1.0;
+
+        [TestMethod]
+        public void OnSegmentTestIsTrue()
+        {
+            System.Windows.Point firstPoint = new System.Windows.Point(firstPointCoordinate, firstPointCoordinate);
+            System.Windows.Point secondPoint = new System.Windows.Point(firstPointCoordinate + secondPointCoordinate / 2, firstPointCoordinate + secondPointCoordinate / 2);
+            System.Windows.Point thirdPoint = new System.Windows.Point(secondPointCoordinate, secondPointCoordinate);
+            
+            Assert.IsTrue(Util.OnSegment(firstPoint, secondPoint, thirdPoint));
+        }
+
+        [TestMethod]
+        public void OnSegmentTestIsFalse()
+        {
+            System.Windows.Point firstPoint = new System.Windows.Point(firstPointCoordinate, firstPointCoordinate);
+            System.Windows.Point secondPoint = new System.Windows.Point(firstPointCoordinate - secondPointCoordinate / 2, firstPointCoordinate - secondPointCoordinate / 2);
+            System.Windows.Point thirdPoint = new System.Windows.Point(secondPointCoordinate, secondPointCoordinate);
+
+            Assert.IsFalse(Util.OnSegment(firstPoint, secondPoint, thirdPoint));
+        }
+
+        [TestMethod]
+        public void OrientationTestAreEqual()
+        {
+            System.Windows.Point firstPoint = new System.Windows.Point(firstPointCoordinate, firstPointCoordinate);
+            System.Windows.Point secondPoint = new System.Windows.Point(firstPointCoordinate + secondPointCoordinate / 2, firstPointCoordinate + secondPointCoordinate / 2);
+            System.Windows.Point thirdPoint = new System.Windows.Point(secondPointCoordinate, secondPointCoordinate);
+
+            Assert.AreEqual(Util.Orientation(firstPoint, secondPoint, thirdPoint), 0);
+        }
+
+        [TestMethod]
+        public void PointTestAreEqual()
+        {
+            WpfShapes.Classes.Point firstPoint = new WpfShapes.Classes.Point();
+            firstPoint.X = firstPointCoordinate;
+            firstPoint.Y = firstPointCoordinate;
+            WpfShapes.Classes.Point secondPoint = new WpfShapes.Classes.Point(firstPointCoordinate, firstPointCoordinate);
+
+            Assert.AreEqual(firstPoint.X, secondPoint.X);
+        }
+
+        [TestMethod]
+        public void ReadHexagonsTestIsNull()
+        {
+            Assert.IsNotNull(Serialization.Deserialize(""));
+        }
+    }
+}
