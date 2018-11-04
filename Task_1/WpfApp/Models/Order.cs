988f1f52fb168d098a81d87f88d69b01f4176d33
Ivan Ternovyi <ivanternovyi@gmail.com>
2018-11-04 06:58:18 +0200

    Base models

---
+++ Task_1/WpfApp/Models/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    class Order
+    {
+        public int Id { get; private set; }
+        public double Price { get; private set; }
+        public string Time { get; private set; }
+        public string Destination { get; private set; }
+        public string Mobile { get; private set; }
+
+        public Order()
+        {
+            Id = 0;
+            Price = 0;
+            Time = "";
+            Mobile = "";
+        }
+
+        public Order(int id, double price, string time, string destination, string mobile)
+        {
+            Id = id;
+            Price = price;
+            Time = time;
+            Mobile = mobile;
+        }
+    }
+}
---
+++ Task_1/WpfApp/Models/TaxiDriver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Models
+{
+    class TaxiDriver
+    {
+        public string Name { get; private set; }
+
+        public List<int> orderIds = new List<int>();
+
+        public TaxiDriver()
+        {
+            Name = "undefined";
+        }
+
+        public TaxiDriver(string name, List<int> orders)
+        {
+            Name = name;
+            orderIds = orders;
+        }
+    }
+}
