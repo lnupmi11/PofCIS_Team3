ff12981fe2f52b2debee6bf33c91920ab42cba00
MaryanTutunnyk <maryan.tutunnyk@gmail.com>
2018-11-20 03:34:51 +0200

    (new) Add UI and functions to assign order to driver and switch betweens drivers.

--- Task_1/WpfApp/MainWindow.xaml.cs
+++ Task_1/WpfApp/MainWindow.xaml.cs
@@ -38,7 +38,7 @@
         /// Method to open file with drivers from file dialog
         /// </summary>
         /// <param name="sender">Sender.</param>
-        /// <param name="e">RoutedEventArgs</param>
+        /// <param name="e">RoutedEventArgs.</param>
         private void OnOpenDriversClick(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -58,7 +58,7 @@
         /// Method to save file with drivers in file dialog menu.
         /// </summary>
         /// <param name="sender">Sender.</param>
-        /// <param name="e">RoutedEventArgs</param>
+        /// <param name="e">RoutedEventArgs.</param>
         private void OnSaveDriversClick(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
@@ -74,7 +74,7 @@
         /// Method to open file with orders from file dialog
         /// </summary>
         /// <param name="sender">Sender.</param>
-        /// <param name="e">RoutedEventArgs</param>
+        /// <param name="e">RoutedEventArgs.</param>
         private void OnOpenOrdersClick(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -89,7 +89,7 @@
         /// Method to save file with orders in file dialog menu.
         /// </summary>
         /// <param name="sender">Sender.</param>
-        /// <param name="e">RoutedEventArgs</param>
+        /// <param name="e">RoutedEventArgs.</param>
         private void OnSaveOrdersClick(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
@@ -124,6 +124,45 @@
         private void UpdateOrdersUI()
         {
             this.allOrders.ItemsSource = this.orders.Where(i => i.Status != "already assigned").ToList();
+        }
+
+        /// <summary>
+        /// Method to assign order to selected driver.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">RoutedEventArgs.</param>
+        private void OnAsignOrderButtonClick(object sender, RoutedEventArgs e)
+        {
+            int selectedOrderIndex = this.allOrders.SelectedIndex;
+            if (selectedOrderIndex != -1)
+            {
+                Order selectedOrder = this.orders.Where(i => i.Status != "already assigned").ToList()[selectedOrderIndex];
+                if (selectedOrder.Status == "not assigned")
+                {
+                    this.selectedTaxiDriver.AssignOrder(selectedOrder);
+
+                    selectedOrder.Status = "already assigned";
+
+                    this.UpdateDriversUI();
+                    this.UpdateSelectedDriverUI();
+                    this.UpdateOrdersUI();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to select driver.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">RoutedEventArgs.</param>
+        private void OnSelectDriverButtonClick(object sender, RoutedEventArgs e)
+        {
+            int selectedTaxiDriverIndex = this.allDrivers.SelectedIndex;
+            if (selectedTaxiDriverIndex != -1)
+            {
+                this.selectedTaxiDriver = this.drivers[selectedTaxiDriverIndex];
+                this.UpdateSelectedDriverUI();
+            }
         }
     }
 }
