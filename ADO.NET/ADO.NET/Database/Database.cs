c9f295ce8728337f2221e96d5171100f2c3919cf
AlexSkorupsky <alex.skorupskyy@gmail.com>
2018-11-27 05:00:12 +0200

    added Database entity

---
+++ ADO.NET/ADO.NET/Database/Database.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NET.Database
+{
+    internal class Database
+    {
+        private readonly string connectionString;
+
+        private SqlConnection connection;
+
+        public Database(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public bool Connect()
+        {
+            try
+            {
+                this.connection = new SqlConnection(this.connectionString);
+                this.connection.Open();
+                if (this.connection.State == ConnectionState.Open)
+                {
+                    return true;
+                }
+            }
+            catch (SqlException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+
+            return false;
+        }
+
+        public bool Disconnect()
+        {
+            try
+            {
+                if (this.connection.State == ConnectionState.Open)
+                {
+                    this.connection.Close();
+                }
+
+                return this.connection.State == ConnectionState.Closed;
+            }
+            catch (SqlException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+
+            return false;
+        }
+    }
+}
