0f2eaf3fe3905b0bce5aa6432e8d2d25bfb828b1
AlexSkorupsky <alex.skorupskyy@gmail.com>
2018-12-02 14:38:04 +0200

    configured connection string and tested Northwind, added Database class

---
+++ ADO.NET/ADO.NET/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.NET.Database
+{
+    internal class Database
+    {
+        private readonly string connectionString;
+
+        private SqlConnection connection;
+
+        public Database(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public bool Connect()
+        {
+            this.connection = new SqlConnection(this.connectionString);
+            this.connection.Open();
+            if (this.connection.State == ConnectionState.Open)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Disconnect()
+        {
+            if (this.connection.State == ConnectionState.Open)
+            {
+                this.connection.Close();
+            }
+            return this.connection.State == ConnectionState.Closed;
+        }
+    }
+}
--- ADO.NET/ADO.NET/Program.cs
+++ ADO.NET/ADO.NET/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace ADO.NET
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ADO.NET
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("All last names of the employees in Northwind");
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT LastName FROM Employees;";
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{0}", reader["LastName"]);
+            }
+            Console.ReadKey();
+            reader.Close();
         }
     }
 }
