736090e293193b3db0db5b6bba1e39c2e8f99d9a
MaryanTutunnyk <maryan.tutunnyk@gmail.com>
2018-12-09 23:33:39 +0200

    (add) UnitOfWork interface and his implementation.

---
+++ Task_1/WpfApp/UOW/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfApp.UOW
+{
+    using WpfApp.Models;
+    using WpfApp.Repository;
+
+    public interface IUnitOfWork
+    {
+        GenericRepository<TaxiDriver> Drivers { get; }
+
+        GenericRepository<Order> Orders { get; }
+    }
+}
---
+++ Task_1/WpfApp/UOW/UnitOfWork.cs
@@ -0,0 +1,67 @@
+﻿namespace WpfApp.UOW
+{
+    using System;
+    using WpfApp.Models;
+    using WpfApp.Repository;
+
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private TaxiAppDbContext context = new TaxiAppDbContext();
+
+        GenericRepository<TaxiDriver> drivers;
+
+        GenericRepository<Order> orders;
+
+        public GenericRepository<TaxiDriver> Drivers
+        {
+            get
+            {
+                if (this.drivers == null)
+                {
+                    this.drivers = new GenericRepository<TaxiDriver>(this.context);
+                }
+
+                return this.drivers;
+            }
+        }
+
+        public GenericRepository<Order> Orders
+        {
+            get
+            {
+                if (this.orders == null)
+                {
+                    this.orders = new GenericRepository<Order>(this.context);
+                }
+
+                return this.orders;
+            }
+        }
+
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    this.context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+    }
+}
